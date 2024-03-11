65fe33ee0e75ba56b8d8be86948b81dab6b4e8ef
filippo cafaro <144951463+FilippoDibe@users.noreply.github.com>
2024-03-11 12:45:07 +0100

    running migrations

--- back-end/database/migrations/2024_03_11_114329_create_apartment_sponsor_table.php
+++ back-end/database/migrations/2024_03_11_114329_create_apartment_sponsor_table.php
@@ -13,7 +13,10 @@ return new class extends Migration
      */
     public function up()
     {
-        //
+        Schema::create('apartment_sponsor', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
     }
 
     /**
@@ -23,6 +26,6 @@ return new class extends Migration
      */
     public function down()
     {
-        //
+        Schema::dropIfExists('apartment_sponsor');
     }
 };
--- back-end/database/migrations/2024_03_11_114346_create_add_foregn_key_table.php
+++ back-end/database/migrations/2024_03_11_114346_create_add_foregn_key_table.php
@@ -13,7 +13,10 @@ return new class extends Migration
      */
     public function up()
     {
-        //
+        Schema::create('add_foregn_key', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
     }
 
     /**
@@ -23,6 +26,6 @@ return new class extends Migration
      */
     public function down()
     {
-        //
+        Schema::dropIfExists('add_foregn_key');
     }
 };
