45bfc9b4062dc4118b189f109815f92e81e8ee2b
Dome1992 <forlanodomenico92@mail.com>
2024-03-16 17:48:16 +0100

    apartmentdetailAPI

--- front-end/src/components/ApartmentDetailApi.vue
+++ front-end/src/components/ApartmentDetailApi.vue
@@ -1,32 +1,31 @@
 <template>
-    <h1>DETTAGLIO APPARTAMENTO:</h1>
-    
-    <div class="container">
+    <div>
+      <h1>DETTAGLIO APPARTAMENTO:</h1>
+      
+      <div class="container">
         <div class="row">
-
-        
-        <div class="col-lg-3 col-md-6">
-             <div class="card my-3">
-                    <img :src="apartment.main_img" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title">{{ apartment.title }}</h5>
-                        <p class="card-text">{{ apartment.description }}</p>
-                        <div class="d-flex justify-content-between">
-                            
-                        </div>
-                    </div>
-                </div>
-            
-    
+          <div class="col-lg-3 col-md-6">
+            <div class="card my-3">
+              <img :src="apartment.main_img" class="card-img-top" alt="...">
+              <div class="card-body">
+                <h5 class="card-title">{{ apartment.title }}</h5>
+                <p class="card-text">{{ apartment.description }}</p>
+                <ul>
+                  <li><strong>Max Guests:</strong> {{ apartment.max_guests }}</li>
+                  <li><strong>Rooms:</strong> {{ apartment.rooms }}</li>
+                  <li><strong>Beds:</strong> {{ apartment.beds }}</li>
+                  <li><strong>Baths:</strong> {{ apartment.baths }}</li>
+                  <li><strong>Address:</strong> {{ apartment.address }}</li>
+                  <!-- <li><strong>Longitude:</strong> {{ apartment.longitude }}</li>
+                  <li><strong>Latitude:</strong> {{ apartment.latitude }}</li> -->
+                </ul>
+              </div>
+            </div>
+          </div>
         </div>
-        
-        </div>
+      </div>
     </div>
-
-
-   
-
-</template>
+  </template>
 
 <script>
 import axios from "axios";
