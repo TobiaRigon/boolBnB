93b7f18aaba6af2fe66f4404a195d5fa1dfda4a6
TobiaRigon <rigontobia@gmail.com>
2024-03-17 17:06:34 +0100

    Fix:Profilo mancante + Testi in italiano

--- back-end/resources/views/profile/partials/delete-user-form.blade.php
+++ back-end/resources/views/profile/partials/delete-user-form.blade.php
@@ -1,30 +1,30 @@
 <section class="space-y-6">
     <header>
         <h2 class="text-lg font-medium text-gray-900">
-            {{ __('Delete Account') }}
+            {{ __('Elimina Account') }}
         </h2>
 
         <p class="mt-1 text-sm text-gray-600">
-            {{ __('Once your account is deleted, all of its resources and data will be permanently deleted. Before deleting your account, please download any data or information that you wish to retain.') }}
+            {{ __('Una volta eliminato il tuo account, tutte le risorse e i dati ad esso associati verranno cancellati definitivamente. Prima di eliminare il tuo account, ti raccomandiamo di scaricare qualsiasi dato o informazione che desideri conservare.') }}
         </p>
     </header>
 
     <x-danger-button
         x-data=""
-        x-on:click.prevent="$dispatch('open-modal', 'confirm-user-deletion')"
-    >{{ __('Delete Account') }}</x-danger-button>
+        x-on:click.prevent="$dispatch('open-modal', 'conferma-eliminazione-utente')"
+    >{{ __('Elimina Account') }}</x-danger-button>
 
-    <x-modal name="confirm-user-deletion" :show="$errors->userDeletion->isNotEmpty()" focusable>
+    <x-modal name="conferma-eliminazione-utente" :show="$errors->userDeletion->isNotEmpty()" focusable>
         <form method="post" action="{{ route('profile.destroy') }}" class="p-6">
             @csrf
             @method('delete')
 
             <h2 class="text-lg font-medium text-gray-900">
-                {{ __('Are you sure you want to delete your account?') }}
+                {{ __('Sei sicuro di voler eliminare il tuo account?') }}
             </h2>
 
             <p class="mt-1 text-sm text-gray-600">
-                {{ __('Once your account is deleted, all of its resources and data will be permanently deleted. Please enter your password to confirm you would like to permanently delete your account.') }}
+                {{ __('Una volta eliminato il tuo account, tutte le risorse e i dati ad esso associati verranno cancellati definitivamente. Inserisci la tua password per confermare che desideri eliminare definitivamente il tuo account.') }}
             </p>
 
             <div class="mt-6">
@@ -43,11 +43,11 @@
 
             <div class="mt-6 flex justify-end">
                 <x-secondary-button x-on:click="$dispatch('close')">
-                    {{ __('Cancel') }}
+                    {{ __('Annulla') }}
                 </x-secondary-button>
 
                 <x-danger-button class="ml-3">
-                    {{ __('Delete Account') }}
+                    {{ __('Elimina Account') }}
                 </x-danger-button>
             </div>
         </form>
