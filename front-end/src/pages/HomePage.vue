2d9951ac1fea53230187aa69e642fa14f28e15e9
TobiaRigon <rigontobia@gmail.com>
2024-03-17 18:00:31 +0100

    HomePage Search base

--- front-end/src/pages/HomePage.vue
+++ front-end/src/pages/HomePage.vue
@@ -1,12 +1,31 @@
 <template>
   <main>
     <div class="container">
-      <h1>HomePage</h1>
-
-      
-
+      <div class="row">
+        <div class="col">
+          <div class="col">
+            <h1>Cerca il tuo appartamento</h1>
+          </div>
+        </div>
+      </div>
+      <form class="form-inline my-2 gap-2 d-flex">
+        <input
+          class="form-control mr-sm-2"
+          type="search"
+          placeholder="Search"
+          aria-label="Search"
+        />
+        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">
+          Search
+        </button>
+      </form>
     </div>
   </main>
 </template>
 
-
+<style>
+.form-control {
+  max-width: 50%;
+  min-width: 700px;
+}
+</style>
