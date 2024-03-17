a02a05e3ed76c7a5af6373f0dc48467a1de7d615
TobiaRigon <rigontobia@gmail.com>
2024-03-17 17:23:28 +0100

    Fix: ApartmentDetail

--- front-end/src/pages/ApartmentDetail.vue
+++ front-end/src/pages/ApartmentDetail.vue
@@ -1,20 +1,17 @@
 <template>
-    <main>
-      <div class="container">
-        <h1>Apartment Details</h1>
-  
-        <ApartmentDetailApi />
-  
-      </div>
-    </main>
-  </template>
-  
-  <script>
-  import ApartmentDetailApi from '../components/ApartmentDetailApi.vue';
-  
-  export default {
-    components: {
-        ApartmentDetailApi,
-    },
-  };
-  </script>
+  <main>
+    <div class="container">
+      <ApartmentDetailApi />
+    </div>
+  </main>
+</template>
+
+<script>
+import ApartmentDetailApi from "../components/ApartmentDetailApi.vue";
+
+export default {
+  components: {
+    ApartmentDetailApi,
+  },
+};
+</script>
