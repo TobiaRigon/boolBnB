c41fea1ee5ff5b8266aa1812f7120eea9f28ac6e
TobiaRigon <rigontobia@gmail.com>
2024-03-18 20:09:56 +0100

    Style provvisorio ApartmentDetailApi

--- front-end/src/components/ApartmentDetailApi.vue
+++ front-end/src/components/ApartmentDetailApi.vue
@@ -4,17 +4,24 @@
 
     <div class="container d-flex justify-content-center">
       <div class="card">
-        <img :src="getImageUrl(apartment.main_img)" class="card-img-top" alt="Immagine dell'appartamento" />
+        <img
+          :src="getImageUrl(apartment.main_img)"
+          class="card-img-top"
+          alt="Immagine dell'appartamento"
+        />
         <div class="card-body">
-          <h5 class="card-title">{{ apartment.title }}</h5>
-          <p class="card-text">{{ apartment.description }}</p>
+          <div class="mb-3">
+            <h5 class="card-title">{{ apartment.title }}</h5>
+            <p class="card-text">{{ apartment.description }}</p>
+          </div>
           <ul>
             <li><strong>Ospiti Massimi:</strong> {{ apartment.max_guests }}</li>
             <li><strong>Camere:</strong> {{ apartment.rooms }}</li>
             <li><strong>Letti:</strong> {{ apartment.beds }}</li>
             <li><strong>Bagni:</strong> {{ apartment.baths }}</li>
             <li><strong>Indirizzo:</strong> {{ apartment.address }}</li>
-            <li><strong>Servizi:</strong>
+            <li>
+              <strong>Servizi:</strong>
               <ul class="list-unstyled">
                 <li v-for="service in apartment.services">
                   <i :class="'fas ' + service.icon"></i> {{ service.name }}
@@ -41,11 +48,14 @@ export default {
   methods: {
     getImageUrl(imagePath) {
       // Controlla se il percorso dell'immagine sembra essere un URL completo
-      if (imagePath && (imagePath.startsWith('http://') || imagePath.startsWith('https://'))) {
+      if (
+        imagePath &&
+        (imagePath.startsWith("http://") || imagePath.startsWith("https://"))
+      ) {
         return imagePath;
       }
       // Altrimenti, costruisci il percorso completo utilizzando il percorso di base del server Laravel
-      const baseUrl = 'http://127.0.0.1:8000'; // Modifica con il tuo URL effettivo se diverso
+      const baseUrl = "http://127.0.0.1:8000"; // Modifica con il tuo URL effettivo se diverso
       return `${baseUrl}/storage/${imagePath}`;
     },
   },
