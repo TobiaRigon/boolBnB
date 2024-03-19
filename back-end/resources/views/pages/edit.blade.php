37014699a2fbee0a2b7a8f6edb3958951c2d01ab
Gabriele Di Rocco <gabriele.dirocco000@gmail.com>
2024-03-19 15:05:07 +0100

    fix edit address

--- back-end/resources/views/pages/edit.blade.php
+++ back-end/resources/views/pages/edit.blade.php
@@ -80,13 +80,7 @@
                             <!-- scegli città -->
 
                             <label for="address" class="form-label"> Indirizzo dell'appartamento</label>
-                            <div class="form-check form-check-inline">
-                                <input class="form-check-input" type="radio" name="city" id="Milano" value="Milano">
-                                <label class="form-check-label" for="Milano">Milano</label>
-                            </div>
-                            <div class="form-check form-check-inline">
-                                <input class="form-check-input" type="radio" name="city" id="Roma" value="Roma">
-                                <label class="form-check-label" for="Roma">Roma</label>
+                        
                             </div>
                             <input type="text" class="form-control" id="address" name="address" value="{{ old('address', $apartment->address) }}" required>
                         </div>
@@ -112,27 +106,11 @@
 
 <script>
 
-   //scegli città
-   //scrivo una funzione che, in base al click mi da diversi valori di lat, long e radius
-   Milano = document.getElementById('Milano')
-   Roma = document.getElementById('Roma')
+
    let lat = '';
    let lon = '';
    let radius = '';
 
-       Milano.addEventListener('change', function () {
-           if (Milano.checked)
-           {lat = '45.4642';
-           lon = '9.1900';
-           radius = '20000';}
-       })
-
-       Roma.addEventListener('change', function () {
-           if (Roma.checked)
-           {lat = '41.9028';
-           lon = '12.4964';
-           radius = '25000';}
-       })
    // chiave API
    const keyApi = 'brzK3He1s61mi6MQycw8qJXnuSAtFOfx';
 
@@ -157,7 +135,7 @@
 
    function searchAdress(adress) {
        fetch(
-               `https://api.tomtom.com/search/2/search/${adress}.json?key=${keyApi}&countrySet=IT&limit=5&lat=${lat}&lon=${lon}&radius=${radius}`
+        `https://api.tomtom.com/search/2/search/${adress}.json?key=${keyApi}`
            )
            .then(response => response.json())
            .then(data => {
