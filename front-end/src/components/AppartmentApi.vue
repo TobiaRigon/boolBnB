7300737476ab35ef4cb6dab9173bedb122e33df8
filippo cafaro <144951463+FilippoDibe@users.noreply.github.com>
2024-03-25 11:32:54 +0100

    fix url e card

--- front-end/src/components/AppartmentApi.vue
+++ front-end/src/components/AppartmentApi.vue
@@ -7,11 +7,11 @@
         v-for="apartment in paginatedList"
         :key="apartment.id"
       >
-          <router-link
-            class="card my-3"
-            :class="{'sponsored-apartment': apartment.in_evidence === 1}"
-            :to="`/apartments/${apartment.id}/${apartment.title}`"
-          >
+      <router-link
+        class="card my-3"
+        :class="{'sponsored-apartment': apartment.in_evidence === 1}"
+        :to="formattedPath(apartment)"
+      >
           <div class="card-container">
             <!-- Usa il metodo getImageUrl per ottenere il corretto percorso dell'immagine -->
             <img
@@ -85,6 +85,10 @@ export default {
       this.currentPage += num;
       console.log(this.currentPage);
     },
+    formattedPath(apartment) {
+    const titleFormatted = apartment.title.toLowerCase().replace(/\s+/g, '-');
+    return `/apartments/${apartment.id}/${titleFormatted}`;
+  }
   },
   mounted() {
     axios
