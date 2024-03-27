ddb62e00f3ed71f95f95478ac466a731ac08ebe5
TobiaRigon <rigontobia@gmail.com>
2024-03-27 10:04:07 +0100

    Fix bordo oro

--- front-end/src/pages/HomePage.vue
+++ front-end/src/pages/HomePage.vue
@@ -26,7 +26,6 @@ export default {
     };
   },
   methods: {
-
     // handleScroll() {
     //             // Gestisce lo scrolling della finestra
     //             if (window.scrollY > 100) { // Imposta un valore di soglia per lo scrolling
@@ -35,8 +34,6 @@ export default {
     //                 this.isScrolled = false; // Altrimenti, imposta isScrolled a false
     //             }
     // },
-          
-
 
     // Metodo per cambiare pagina
     changePage(num) {
@@ -51,7 +48,6 @@ export default {
         .replace(/\-\-+/g, "-"); // Sostituisci multipli trattini con un singolo trattino
     },
 
-    
     // metodo per autocomplete tomtom
     autoComplete() {
       this.showAutoComplete = true;
@@ -210,14 +206,13 @@ export default {
 
 <template>
   <main>
-
-    
     <div class="container-fluid">
       <!-- Jumbo + ricerca -->
 
-     
-
-      <div class="jumbotron d-flex align-items-center justify-content-center" id="top">
+      <div
+        class="jumbotron d-flex align-items-center justify-content-center"
+        id="top"
+      >
         <div class="container">
           <div class="row">
             <div class="col">
@@ -271,48 +266,53 @@ export default {
       </div>
 
       <div v-show="isScrolled">
-          <a href="#top">
-              <i class="fa-solid fa-arrow-up-long"></i>
-          </a>
+        <a href="#top">
+          <i class="fa-solid fa-arrow-up-long"></i>
+        </a>
       </div>
 
       <!-- Roba nuova -->
-     
+
       <div class="container text-center my-5">
-    <div class="row justify-content-center rounded bg-white shadow-lg">
-        <div class="col-md-4">
+        <div class="row justify-content-center rounded bg-white shadow-lg">
+          <div class="col-md-4">
             <div class="text-center p-4">
-                <i class="fa-solid fa-earth-americas fa-3x"></i>
-                <h5 class="font-weight-bold mt-4 mb-3">Trova l'ispirazione per il tuo viaggio</h5>
-                <p class="mb-4 text-muted">
-                    Sfoglia case vacanza, chalet, case al mare, appartamenti, condomini, case galleggianti, castelli, agriturismi e tutto il resto.
-                </p>
+              <i class="fa-solid fa-earth-americas fa-3x"></i>
+              <h5 class="font-weight-bold mt-4 mb-3">
+                Trova l'ispirazione per il tuo viaggio
+              </h5>
+              <p class="mb-4 text-muted">
+                Sfoglia case vacanza, chalet, case al mare, appartamenti,
+                condomini, case galleggianti, castelli, agriturismi e tutto il
+                resto.
+              </p>
             </div>
-        </div>
+          </div>
 
-        <div class="col-md-4">
+          <div class="col-md-4">
             <div class="text-center p-4">
-                <i class="fa-solid fa-user-check fa-3x"></i>
-                <h5 class="mt-4 mb-3">Scopri milioni di offerte</h5>
-                <p class="mb-4 text-muted">
-                    Trova e confronta offerte uniche da migliaia di partner fidati. BoolBnB ha la più vasta selezione di case vacanza in tutto il mondo.
-                </p>
+              <i class="fa-solid fa-user-check fa-3x"></i>
+              <h5 class="mt-4 mb-3">Scopri milioni di offerte</h5>
+              <p class="mb-4 text-muted">
+                Trova e confronta offerte uniche da migliaia di partner fidati.
+                BoolBnB ha la più vasta selezione di case vacanza in tutto il
+                mondo.
+              </p>
             </div>
-        </div>
+          </div>
 
-        <div class="col-md-4">
+          <div class="col-md-4">
             <div class="text-center p-4">
-                <i class="fa-solid fa-house fa-3x"></i>
-                <h5 class="mt-4 mb-3">Prenota l'alloggio perfetto</h5>
-                <p class="mb-4 text-muted">
-                    Con BoolBnB è facile e veloce prenotare la sistemazione giusta per qualsiasi viaggio, indipendentemente dal tuo budget.
-                </p>
+              <i class="fa-solid fa-house fa-3x"></i>
+              <h5 class="mt-4 mb-3">Prenota l'alloggio perfetto</h5>
+              <p class="mb-4 text-muted">
+                Con BoolBnB è facile e veloce prenotare la sistemazione giusta
+                per qualsiasi viaggio, indipendentemente dal tuo budget.
+              </p>
             </div>
+          </div>
         </div>
-    </div>
-</div>
-
-
+      </div>
 
       <!-- Appartamenti in evidenza -->
 
@@ -451,10 +451,4 @@ h1 {
   transform: scale(1.02); /* Applica una leggera trasformazione di scala */
   box-shadow: 0 0 10px rgba(0, 0, 0, 0.1); /* Aggiungi un'ombra sottile */
 }
-.sponsored-apartment {
-  border: 2px solid #ffd700; /* Giallo Oro per sponsorizzazione */
-}
-/* .kilometri {
-  width: 70px;
-} */
 </style>
